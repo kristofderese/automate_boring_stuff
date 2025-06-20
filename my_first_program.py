b12d06f809d545934a7395a9b4323eac4bb5ff7e
kristofderese <kristof.de.rese@gmail.com>
2025-06-20 21:05:15 +0200

    my_first_program full

---
+++ my_first_program.py
@@ -0,0 +1,8 @@
+print("Hello world!")
+print("What is your name?")
+user_name = input ("What is your name?   ")
+print("It is good to meet you, " + user_name + "!")
+print("The length of your name is:" + str(len(user_name)))
+user_age = int(input("What is your age?   "))
+age_oneyear = user_age + 1
+print("You will be " + str(age_oneyear) + " in one year.")
