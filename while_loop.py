0f9e01ef92fe39370db3af4fcd713d86f6935317
kristofderese <kristof.de.rese@gmail.com>
2025-06-20 21:43:48 +0200

    while loop

---
+++ while_loop.py
@@ -0,0 +1,20 @@
+spam = 0
+if spam < 5:
+    print("Spaham")
+    spam = spam +1
+
+jos = 0
+while jos < 5:
+    print ("Johos")
+    jos = jos +1
+
+while True:
+    name = input("Who are you?  ")
+    if name != "Jos":
+        continue
+    print ("Helle Jos! What is the password?   ")
+    pw = input("<<  ")
+    if pw == "lol":
+        break
+print("Access granted!")
+
